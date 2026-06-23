0585ff6431e8db5ac56e30dbb8d4a0e9922cd936
Arzish Maneckji <arzishmaneckji0@gmail.com>
2026-06-23 15:17:39 +0530

    removing garbage

--- sales/src/components/Skeleton.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export function Skeleton({ className = '' }) {
-    return _jsx("div", { className: `animate-pulse bg-gray-200 dark:bg-gray-700 rounded ${className}` });
-}
